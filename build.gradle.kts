36725d90b8a21008e928eb87e8e3f288266dd21f
Yehor Lashkul <yegor.lashkul@gmail.com>
2020-12-08 17:53:56 +0200

    add gradle

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    java
+    idea
+}
+
+repositories {
+    jcenter()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
